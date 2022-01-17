5ea2a835183583dde23071ab7c756a8d5ac49daf
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-01-17 20:46:42 +0100

    commit 17-1-2022

--- main.py
+++ main.py
@@ -1,152 +1,147 @@
 __winc_id__ = "d7b474e9b3a54d23bca54879a4f1855b"
 __human_name__ = "Betsy Webshop"
 
-from models import *
-from create_data import *
-from datetime import *
+
+from peewee import *
+from datetime import datetime
+import models 
+
+def search(term=str):
+    term = term.lower()
+    query = models.Product.select().where(models.Product.name.contains(term) | models.Product.description.contains(term))
+
+    if query:
+        print('Search results:')
+        for product in query:
+            print(product.name)
+    else:
+        print('No products matched your search term.')
 
 
-db.connect()
-db.create_tables([User, Tag, Product, UserProduct, Transaction])
+def list_user_products(user_id=int):
+    query = models.Product.select().where(models.Product.owner == user_id)
 
-    
-def search(term):
-    search_term = term.lower()
-    match = Product.select().where(
-        (fn.lower(Product.name).contains(search_term)) | (fn.lower(Product.description).contains(search_term))
+    if query:
+        user = models.User.get_by_id(user_id)
+
+        print(user.name + "'s products:")
+
+        for product in query:
+            print(product.name)
+    else:
+        print('Either the user has no products or no valid id was given.')
+
+
+def list_products_per_tag(tag_id=int):
+    query = models.Product.select().join(models.ProductTag).join(models.Tag).where(models.Tag.id == tag_id)
+
+    if query:
+        tag = models.Tag.get_by_id(tag_id)
+
+        print('All products associated with ' + tag.name + ':')
+
+        for product in query:
+            print(product.name)
+    else:
+        print('Either the tag has no associated products or no valid id was given.')
+
+
+def add_product_to_catalog(user_id=int, product=models.Product):
+    user = models.User.get_by_id(user_id)
+    product.owner = user
+    product.save()
+    print(product.name + ' with the id of ' + str(product.id) + ' owned by ' + user.name + ' was stored in the database.')
+
+
+def update_stock(product_id=int, new_quantity=int):
+    product = models.Product.get_by_id(product_id)
+    old_stock = product.quantity
+    product.quantity = new_quantity
+    product.save()
+    print(product.name + ' used to have ' + str(old_stock) + ' in stock. New stock is: ' + str(product.quantity) + '.')
+
+
+def purchase_product(product_id=int, buyer_id=int, quantity=int):
+    product = models.Product.get_by_id(product_id)
+    buyer = models.User.get_by_id(buyer_id)
+
+    if buyer.id == product.owner:
+        print('You cannot buy products from yourself ' + buyer.name + '.')
+        return
+
+    if quantity >= product.quantity:
+        print('Not enough of ' + product.name + ' in stock.')
+        return
+
+    total_price = round(product.price * quantity, 2)
+
+    transaction = models.Transaction.create(
+        buyer = buyer.id,
+        bought_product = product.id,
+        quantity = quantity,
+        total_price = total_price,
+        bought_at = datetime.now()
     )
-    if match:
-        print(f"The term {term} has been matched to:")
-        for product in match:
-            print(product.name)
-    else:
-        print('Product not found')
+
+    print('At ' + str(transaction.bought_at) + ', ' + buyer.name + ' bought ' + str(transaction.quantity) + ' of ' + product.name + ' at a total price of: €' + str(transaction.total_price) + '.')
+
+    new_quantity = product.quantity - quantity
+
+    update_stock(product.id, new_quantity)
 
 
-# View the products of a given user.
-def list_user_products(user_id):
-    product_list = UserProduct.select().where(UserProduct.owner == user_id)
-
-    if product_list:
-        print(f"{user_id} has:")
-        for product in product_list:
-            print(product.product)
-    else:
-        print('The owner does not exist or has no products.')
-
-
-# View all products for a given tag.
-def list_products_per_tag(tag_id):
-    product_list = Product.select().where(Product.tags == tag_id)
-
-    if product_list:
-        print(f"These are the products tagged {tag_id}:")
-        for product in product_list:
-            print(product.name)
-    else:
-        print('The tag does not exist or has no products attached.')
-
-
-# Add a product to a user.
-def add_product_to_catalog(user_id, product):
-    product_to_get = Product.select().where(Product.name == product)
-    new_owner = User.select().where(User.name == user_id)
-
-    if product_to_get and new_owner:
-        added_product = UserProduct.create(owner=user_id,product=product,quantity=1,tags=Product.tags
-        )
-        print('New product added.')
-        return added_product
-    else:
-        print('Person or product not found.')
-
-
-# Remove a product from a user.
 def remove_product(product_id):
-    try:
-        product = UserProduct.get(UserProduct.product == product_id)
-        print(f"The product {product_id} has been removed.")
-        return product.delete_instance()
-    except DoesNotExist:
-        print('Can not find product.')
+    product = models.Product.get_by_id(product_id)
+    print('Deleting ' + product.name + ' from the database.')
+    product.delete_instance()
 
 
-# Update the stock quantity of a product.
-def update_stock(product_id, new_quantity):
-    product_to_change = Product.get(Product.name == product_id)
 
-    if product_to_change:
-        print(f'Old amount {product_to_change.name}: {product_to_change.quantity}')
-        product_to_change.quantity = new_quantity
-        product_to_change.save()
-        print(f'New amount {product_to_change.name}: {product_to_change.quantity}')
-    else:
-        print('Can not find product')
+def main():
+    # Init database
+    # models.init()
+    # print('')
 
+    # # Fill with Test Data
+    # models.test_data()
+    # print('')
 
-# Handle a purchase between a buyer and a seller for a given product
-def purchase_product(product_id, buyer_id, quantity):
-    buyer = User.get(User.name == buyer_id)
-    product_to_buy = Product.get(Product.name == product_id)
+    # Search
+    # search('sweater')
+    # print('')
+
+    # List User Products
+    # list_user_products(2)
+    # print('')
+
+    # # List all products tagged with '??'
+    # list_products_per_tag(1)
+    # print('')
+
+    # # starts selling 
+    # product = models.Product(name='Olive Oil', description='Fresh olive oil from the farm', price=6.50, quantity=10)
+    # add_product_to_catalog(2, product)
+    # print('')
+
+    # # List John's Products again
+     list_user_products(2)
+     print('')
+
+    # # Now there's only 5 thinks left in the webshop
+    # update_stock(5, 5)
+    # print('')
+
+    # # Let's make a transaction
+    # purchase_product(1, 2, 2)
+    # print('')
+
+    # # And remove a product
+    # remove_product(6)
+    # print('')
+
     
-    if buyer and product_to_buy:
-        current_date = datetime.now().date()
-        get_date = datetime.strftime(current_date, '%d-%m-%Y')
-        check_amount = product_to_buy.quantity - quantity
-        # check if quantity exists
-        if check_amount >= 0:
-            
-            # add a new transaction to the db
-            transaction = Transaction.create(
-                date=get_date,
-                user=buyer_id,
-                product=product_id,
-                quantity=quantity
-            )
-            print(f"Transaction complete: {transaction.date}, {transaction.user}")
-
-            add_product_to_catalog(buyer_id, product_id)
-            list_user_products(buyer_id)
-            
-            if check_amount == 0:
-                # delete product
-                return remove_product(product_id)
-            # update
-            return update_stock(product_id, check_amount)    
-        else:
-            print('Amount not in stock.')
-    else:
-        print('User or product not found.')    
-
-if __name__ == "__main__":
-    pass
 
 
-
-   
-    
-# <<<<<<<<<: search try out :>>>>>>>>>>
-
-# search('sweater')
-# search('Espresso machine')
-
-# list_user_products('Kees')
-# list_user_products('Jeroen')
-
-# list_products_per_tag('Books')
-# list_products_per_tag('Clothes')
-    
-# list_user_products('Kees')
-
-# list_user_products('Jeroen')
-
-# add_product_to_catalog('Kees', 'sweater')
-# list_user_products('Kees')
-
-
-# update_stock('code Book', 2)
-
-# purchase_product('code Book', 'Kees', 10)
-# purchase_product('Espresso machine', 'Jeroen', 1)
-
-
+if __name__ == '__main__':
+    main()
+  
--- models.py
+++ models.py
@@ -1,52 +1,192 @@
 from peewee import *
+import sqlite3
+connection = sqlite3.connect('betsy.db')
 
-db = SqliteDatabase('betsy.db', pragmas=(
-    ('cache_size', -1024 * 64),  # 64MB page-cache.
-    ('journal_mode', 'wal'),  # Use WAL-mode (you should always use this!).
-    ('foreign_keys', 1)))  # Enforce foreign-key constraints.
+db = SqliteDatabase('betsy.db')
 
 
-class DBmodel(Model):
+class User(Model):
+    name = CharField()
+
     class Meta:
         database = db
 
 
-# A user has a name, address data, and billing information.
-class User(DBmodel):
-    name = CharField()
-    address = CharField()
-    location = CharField()
+class User_Address(Model):
+    user = ForeignKeyField(User, backref='address')
+    street = CharField()
     postal_code = CharField()
-    credit_card= IntegerField()
+    city = CharField()
+
+    class Meta:
+        database = db
 
 
-# In order to facilitate search and categorization, a product must have a number of descriptive tags.
-class Tag(DBmodel):
-    tag = CharField(unique=True)
+class User_Billing(Model):
+    user = ForeignKeyField(User, backref='billing')
+    card_type = CharField()
+    card_number = IntegerField()
+
+    class Meta:
+        database = db
 
 
-# The products must have a name, a description, a price per unit, and a quantity describing the amount in stock.
-class Product(DBmodel):
+class Product(Model):
+    owner = ForeignKeyField(User, backref='products')
     name = CharField()
     description = CharField()
     price = FloatField()
     quantity = IntegerField()
-    tags = ForeignKeyField(Tag, backref='p_tag')
 
-class UserProduct(DBmodel):
-    owner = CharField()
-    product = CharField()
+    class Meta:
+        database = db
+
+
+class Tag(Model):
+    name = CharField()
+
+    class Meta:
+        database = db
+
+
+class ProductTag(Model):
+    product = ForeignKeyField(Product)
+    tag = ForeignKeyField(Tag)
+
+    class Meta:
+        database = db
+
+
+class Transaction(Model):
+    buyer = ForeignKeyField(User, backref='transactions')
+    bought_product = ForeignKeyField(Product, backref='transactions')
     quantity = IntegerField()
-    tags = ForeignKeyField(Tag, backref='up_tag')
+    total_price = FloatField()
+    bought_at = DateTimeField()
+
+    class Meta:
+        database = db
 
 
-# We want to be able to track the purchases made on the marketplace, therefore a transaction model must exist
-class Transaction(DBmodel):
-    # date could be DateField, but instead I've used datetime module to create a string of the date.
-    date = CharField()
-    user = CharField()
-    product = CharField()
-    quantity = IntegerField()
+def init():
+    connection = db.connect()
+
+    if connection:
+        print('Connected to database.')
+
+    with db:
+        db.create_tables([User,User_Address,User_Billing,Product,Tag,ProductTag,Transaction])
+
+        print('Created tables.')
+
+
+def test_data():
+    # User1 - lizzy
+    lizzy = User.create(
+        name='lizzy'
+    )
+
+    lizzy_address = User_Address.create(
+        user=lizzy, 
+        street='Hoofdweg 34', 
+        postal_code='1104 MM', 
+        city='Amsterdam'
+    )
+
+    lizzy_billing = User_Billing.create(
+        user=lizzy, 
+        card_type='Rabobank', 
+        card_number=12345678
+    )
+
+    sweater = Product.create(
+        owner=lizzy, 
+        name='sweater', 
+        description='Warm home sweater handmade by grandma Betsy',
+        price=20.99,
+        quantity=3
+    )
+
+    socks = Product.create(
+        owner=lizzy,
+        name='socks',
+        description='Warm home socks handmade by grandma Betsy',
+        price=8.99,
+        quantity=5
+    )
+
+    hat = Product.create(
+        owner=lizzy,
+        name='hat',
+        description='Warm home hat handmade by grandma',
+        price=14.99,
+        quantity=3
+    )
+
+    # User2 - kees
+    kees = User.create(
+        name='kees'
+    )
+
+    kees_address = User_Address.create(
+        user=kees,
+        street='Bijstraat 70',
+        postal_code='1188 AB',
+        city='Leiden'
+    )
+
+    kees_billing = User_Billing.create(
+        user=kees,
+        card_type='ABN Amro',
+        card_number=87654321
+    )
+
+    sausages = Product.create(
+        owner=kees,
+        name='Sausages',
+        description='Fresh sausages from the farm',
+        price=1.99,
+        quantity=25
+    )
+
+    steak = Product.create(
+        owner=kees,
+        name='Steak',
+        description='Fresh steak from the farm',
+        price=7.45,
+        quantity=10
+    )
+
+    chicken_drums = Product.create(
+        owner=kees,
+        name='Chicken Drums',
+        description='Fresh chicken drums from the farm',
+        price=4.99,
+        quantity=20
+    )
+
+    #Tags
     
-if __name__ == "__main__":
-    pass
+    clothing= Tag.create(name='clothing')
+
+    food = Tag.create(name='food')
+
+   
+
+   
+    # lizzy
+    
+    ProductTag.create(product=sweater,tag=clothing)
+
+    ProductTag.create(product=socks,tag=clothing)
+
+    ProductTag.create(product=hat,tag=clothing)    
+
+    # kees
+    ProductTag.create(product=sausages,tag=food)
+
+    ProductTag.create( product=steak,tag=food)
+
+    ProductTag.create(product=chicken_drums,tag=food)
+    
+    print('Database filled with data.')
